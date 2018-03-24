5da7d7d8eb96e3908f0991f3cb2fbb7c68247a6f
Turing1887 <tim.zschage@t-online.de>
2018-03-24 18:00:31 +0100

    stand jetzt

--- Gamejam_2018/Assets/PlayerMovement.cs
+++ Gamejam_2018/Assets/PlayerMovement.cs
@@ -11,10 +11,7 @@ public class PlayerMovement : MonoBehaviour {
     void Start () {
         rig = GetComponent<Rigidbody2D>();
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> 40a5e446cb8ba7bfbab7081f003c800a7928dca3
 	void Update () {
         if(gameObject.tag == "Player_1")
         {
