3b67d9645bd16516387bc354bff8e46fd52405e2
DESKTOP-HONDUMS\Vincent <vschnoor@mc-kiel.de>
2018-03-24 18:53:26 +0100

    merge

--- Gamejam_2018/Assets/Scripts/Tubes.cs
+++ Gamejam_2018/Assets/Scripts/Tubes.cs
@@ -7,16 +7,28 @@ public class Tubes : MonoBehaviour {
 	string tube;
 	public bool isLocked = false;
 	public string lockedPlayer;
+	public GameController contr;
 
 	// Use this for initialization
 	void Start () {
 		tube = gameObject.tag;
+		contr = GameObject.Find ("GameManager").GetComponent<GameController>();
 	}
 
 	void OnTriggerEnter2D(Collider2D coll){
 		if(!isLocked){
-			coll.gameObject.SetActive (false);
+//			coll.gameObject.SetActive (false);
+			Destroy(coll.gameObject);
 			lockedPlayer = coll.gameObject.tag;
+			if (tube == "Tube_1") {
+				contr.tubes [0] = lockedPlayer;
+			}else if(tube == "Tube_2"){
+				contr.tubes [1] = lockedPlayer;
+			}else if(tube == "Tube_3"){
+				contr.tubes [2] = lockedPlayer;
+			}else if(tube == "Tube_4"){
+				contr.tubes [3] = lockedPlayer;
+			}
 			isLocked = true;
 		}
 	}
