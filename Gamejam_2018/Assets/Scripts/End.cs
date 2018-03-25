b3f5b7d636fc1e9ba52bb6a92d76be9f1b3b7515
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2018-03-25 15:41:00 +0200

    End_Room
    when last Player hit the Egg -  Game fades to white.

---
+++ Gamejam_2018/Assets/Scripts/End.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.tag == "Player_1" || other.gameObject.tag == "Player_2" || other.gameObject.tag == "Player_3" || other.gameObject.tag == "Player_4") 
+		{
+			Destroy(other.gameObject);
+			anim.SetTrigger ("fade");
+		}
+	}
+}
